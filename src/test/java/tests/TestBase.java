8a608f09111c2658157a4bc89ca14fabe02dd919
Alina K <alina885@mail.ru>
2025-07-31 17:12:19 +0300

    Initial commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,57 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.BrowserstackDriver;
+import drivers.EmulationDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        String deviceHost = System.getProperty("deviceHost", "local");
+
+        if (deviceHost.equals("browserstack")) {
+            Configuration.browser = BrowserstackDriver.class.getName();
+        } else {
+            Configuration.browser = EmulationDriver.class.getName();
+        }
+        Configuration.browserSize = null;
+        Configuration.timeout = 30000;
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void addAttachments() {
+
+        String deviceHost = System.getProperty("deviceHost", "local");
+        String sessionId = Selenide.sessionId().toString();
+
+        Attach.pageSource();
+        Attach.screenshotAs("Last screenshot");
+
+        if (deviceHost.equals("browserstack")) {
+            Attach.addVideo(sessionId);
+        }
+        closeWebDriver();
+    }
+}
